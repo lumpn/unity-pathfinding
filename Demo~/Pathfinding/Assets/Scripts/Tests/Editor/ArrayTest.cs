d477901e3b5d4fc36ad6a18e2519dc808f9a24f9
lumpn <lumpn@users.noreply.github.com>
2021-11-07 23:16:08 +0800

    Added performance tests

---
+++ Demo~/Pathfinding/Assets/Scripts/Tests/Editor/ArrayTest.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Diagnostics;
+using NUnit.Framework;
+
+namespace Lumpn.Pathfinding.Demo
+{
+    [TestFixture]
+    public sealed class ArrayTest
+    {
+        private const int length = 32 * 1024 * 1024;
+
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [Test]
+        public void TestClear()
+        {
+            var items = new int[length];
+
+            var watch = new Stopwatch();
+            watch.Start();
+
+            Array.Clear(items, 0, length);
+
+            watch.Stop();
+
+            UnityEngine.Debug.LogFormat("Clear took {0} ms", watch.ElapsedMilliseconds);
+        }
+
+        [Test]
+        public void TestFill()
+        {
+            var items = new int[length];
+
+            var watch = new Stopwatch();
+            watch.Start();
+
+            Fill(items, -1, 0, length);
+
+            watch.Stop();
+
+            UnityEngine.Debug.LogFormat("Clear took {0} ms", watch.ElapsedMilliseconds);
+        }
+
+        private static void Fill<T>(T[] array, T value, int startIndex, int count)
+        {
+            int endIndex = startIndex + count;
+            for (int i = startIndex; i < endIndex; i++)
+            {
+                array[i] = value;
+            }
+        }
+    }
+}
